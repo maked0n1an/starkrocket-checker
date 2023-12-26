61e1af6a4933793c2b2e2337f208fa4dd819f078
maked0n1an <amedik733@gmail.com>
2023-12-26 16:00:09 +0200

    Added headers

--- data/checker.py
+++ data/checker.py
@@ -14,11 +14,24 @@ class Checker(Wallet):
         params = {
             'address': self.address
         }
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'ru,en;q=0.9,ru-RU;q=0.8,zh-TW;q=0.7,zh;q=0.6',
+            'Connection': 'keep-alive',
+            'Referer': 'https://starkrocket.xyz/airdrop',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+        }
         
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url=url,
-                                    params=params, proxy=self.proxy) as response:
+                                    params=params, headers=headers,proxy=self.proxy) as response:
                     if response.status == 200:
                         return await self._get_json(response)        
                     else:
